3772e428da9e44aeaf7c922a8a13c468f192131c
Ashutosh Mishra <ashutoshrm01@gmail.com>
2021-09-13 18:00:48 +0530

    added area

---
+++ area.js
@@ -0,0 +1,15 @@
+const sides = document.querySelectorAll(".side-input");
+const areaButton = document.querySelector("#area-btn");
+const outputDiv = document.querySelector("#output");
+
+function calculateArea() {
+    var area = areaOfTriangle(Number(sides[0].value), Number(sides[1].value));
+    outputDiv.innerText= "The area of Triangle is : " + area;
+}
+
+function areaOfTriangle(b,h) {
+    const area = (b*h)/2;
+    return area;
+}
+
+areaButton.addEventListener("click", calculateArea);
