3a5090a259f4ffcd44cadded98d8daf4ae74fca8
Ashutosh Mishra <ashutoshrm01@gmail.com>
2021-09-19 19:22:02 +0530

    negative value

--- isTriangle.js
+++ isTriangle.js
@@ -8,7 +8,7 @@ function calculateSumOfAngles(angle1,angle2,angle3) {
 }
 
 function isTriangle() {
-    if(inputs.value > 0) {
+    if(inputs.value > -1) {
     const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
         if(sumOfAngles===180) {
             outputDiv.innerText = "Yay! We have a Triangle";
@@ -17,8 +17,8 @@ function isTriangle() {
             outputDiv.innerText = "Oh no, We can't make Triangle with these angles";
         }
     } else {
-        outputDiv.innerText = "Values can't be negative !!"
-    }
+        outputDiv.innerText = "Values can't be negative"
+    };
 }
 
 isTriangleBtn.addEventListener("click", isTriangle);
