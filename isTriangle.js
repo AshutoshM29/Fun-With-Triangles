eaba0e6515351c09f25634cc87c2e29582e03da1
Ashutosh Mishra <ashutoshrm01@gmail.com>
2021-09-20 15:46:43 +0530

    refactor : var

--- isTriangle.js
+++ isTriangle.js
@@ -1,14 +1,15 @@
-const inputs = document.querySelectorAll(".angle-input");
-const isTriangleBtn = document.querySelector("#is-triangle-btn");
-const outputDiv = document.querySelector("#output");
+var inputs = document.querySelectorAll(".angle-input");
+var isTriangleBtn = document.querySelector("#is-triangle-btn");
+var outputDiv = document.querySelector("#output");
+var formInput = document.querySelector("#form-input")
 
 function calculateSumOfAngles(angle1,angle2,angle3) {
-    const sumOfAngles = angle1+angle2+angle3;
+    var sumOfAngles = angle1+angle2+angle3;
     return sumOfAngles;
 }
 
 function isTriangle() {
-    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
+    var sumOfAngles = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
         if(sumOfAngles===180) {
             outputDiv.innerText = "Yay! We have a Triangle";
         }
